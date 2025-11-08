e21b10614a23695787e1f605a1aa042354a24826
gianluL <141229539+gianluL@users.noreply.github.com>
2025-11-08 19:02:56 +0100

    Add files via upload

---
+++ API/api.py
@@ -0,0 +1,42 @@
+<!doctype html><html lang="it"><head><title>Controllo se sei un robot...</title><link rel="stylesheet" href="/.within.website/x/xess/xess.min.css?cachebuster=v1.22.0"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="noindex,nofollow"><style>
+        body,
+        html {
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .centered-div {
+            text-align: center;
+        }
+
+        #status {
+            font-variant-numeric: tabular-nums;
+        }
+
+        #progress {
+          display: none;
+          width: 90%;
+          width: min(20rem, 90%);
+          height: 2rem;
+          border-radius: 1rem;
+          overflow: hidden;
+          margin: 1rem 0 2rem;
+					outline-offset: 2px;
+					outline: #b16286 solid 4px;
+				}
+
+        .bar-inner {
+            background-color: #b16286;
+            height: 100%;
+            width: 0;
+            transition: width 0.25s ease-in;
+        }
+    	</style><script id="anubis_version" type="application/json">"v1.22.0"
+</script><script id="anubis_challenge" type="application/json">{"rules":{"algorithm":"fast","difficulty":2,"report_as":2},"challenge":{"id":"019a64a2-3b3c-7479-9b9b-0b8a10e6d60a","method":"fast","randomData":"ce0a6943d530d312507de28663d5a22f15dbdee93e4091e220cf51682278324df90e900ae9ea1f5100111450dd3a048d5d7799b6daa34e3b2eb30cdc95a8be83","issuedAt":"2025-11-08T18:02:24.956304732Z","metadata":{"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36","X-Real-Ip":"52.18.49.203"},"spent":false}}
+</script><script id="anubis_base_prefix" type="application/json">""
+</script><script id="anubis_public_url" type="application/json">""
+</script></head><body id="top"><main><h1 id="title" class="centered-div">Controllo se sei un robot...</h1><div class="centered-div"><img id="image" style="width:100%;max-width:256px;" src="/.within.website/x/cmd/anubis/static/img/pensive.webp?cacheBuster=v1.22.0"> <img style="display:none;" style="width:100%;max-width:256px;" src="/.within.website/x/cmd/anubis/static/img/happy.webp?cacheBuster=v1.22.0"><p id="status">Caricamento...</p><script async type="module" src="/.within.website/x/cmd/anubis/static/js/main.mjs?cacheBuster=v1.22.0"></script><div id="progress" role="progressbar" aria-labelledby="status"><div class="bar-inner"></div></div><details><p>Vedi questa schermata perché l&#39;amministratore di questo sito web ha installato Anubis per proteggere il server dalla piaga delle aziende di AI generativa che estraggono, senza freno, dati dai siti web. Questo comportamento causa disservizi per i siti web, rendendoli inaccessibili a tutti.</p><p>Anubis è un compromesso. Anubis utilizza un meccanismo di proof-of-work in stile Hashcash, un meccanismo per ridurre le email di spam. L&#39;idea è che, a livello individuale, il lavoro aggiuntivo necessario per la proof-of-work sia trascurabile, ma, a livello di grandi reti di bot, il lavoro si somma e diventa molto più costoso.</p><p>In definitiva, questa è una soluzione provvisoria in modo che si possa dedicare più tempo all&#39;identificazione e al rilevamento dei browser headless (ad esempio, tramite il modo in cui rendono i caratteri) in modo che la pagina di prova del lavoro non debba essere presentata agli utenti che sono molto più propensi a essere legittimi.</p><p>Si noti che Anubis richiede l&#39;utilizzo di caratteristiche moderne di JavaScript che alcuni plugin, come JShelter, disabilitano. Per accedere, disabilita JShelter (o altri plugin simili) per questo dominio.</p></details><noscript><p>Purtroppo, devi abilitare Javascript per riuscire a superare questa pagina. Questa misura è necessaria perché alcune compagnie di AI hanno unilateralmente deciso di violare il contratto sociale sulla fornitura di siti web. Stiamo lavorando ad una soluzione che non richieda Javascript.</p></noscript><div id="testarea"></div></div><footer><div class="centered-div"><p>Protetto da <a href="https://github.com/TecharoHQ/anubis">Anubis</a> From <a href="https://techaro.lol">Techaro</a>. Realizzato con ❤️ in 🇨🇦.</p><p>Mascotte disegnata da <a href="https://bsky.app/profile/celphase.bsky.social">CELPHASE</a>.</p><p>Questo sito sta usando Anubis versione <code>v1.22.0</code>.</p></div></footer></main></body></html>
